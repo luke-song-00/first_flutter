9da4155a49d75c28b716d3c15c053ff0a5fcd688
luke.song <luke.song.00@gmail.com>
2022-03-20 03:08:34 +0900

    init commit - part 2

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+import "package:my_app/random_words.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Startup Name Generator",
+      home: RandomWords(),
+    );
+  }
+}
